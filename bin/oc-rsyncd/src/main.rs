68193204653f40e2250ef9ee0882bb70f6e78581
Ofer Chen <oferchen@gmail.com>
2025-10-30 03:56:17 +0100

    Add Windows GNU unwind compatibility shim (#1636)

--- bin/oc-rsyncd/src/main.rs
+++ bin/oc-rsyncd/src/main.rs
@@ -3,6 +3,9 @@
 use std::{env, io, process::ExitCode};
 
 fn main() -> ExitCode {
+    #[cfg(all(target_os = "windows", target_env = "gnu"))]
+    rsync_windows_gnu_eh::force_link();
+
     let mut stdout = io::stdout().lock();
     let mut stderr = io::stderr().lock();
     let status = rsync_daemon::run(env::args_os(), &mut stdout, &mut stderr);
