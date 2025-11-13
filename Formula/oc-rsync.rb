49d66808ed8f44b0c3fe1df886b673f39d323403
Ofer Chen <oferchen@gmail.com>
2025-11-13 11:00:25 +0100

    Refine cross-platform packaging workflows

--- Formula/oc-rsync.rb
+++ Formula/oc-rsync.rb
@@ -1,8 +1,8 @@
 class OcRsync < Formula
   desc "Pure-Rust rsync 3.4.1-compatible implementation"
   homepage "https://github.com/oferchen/rsync"
-  version "v0.0.0-local"
-  url "https://github.com/oferchen/rsync/releases/download/v0.0.0-local/oc-rsync-macos-amd64.tar.gz"
+  version "0.0.0-local"
+  url "https://github.com/oferchen/rsync/releases/download/v0.0.0-local/oc-rsync-0.0.0-local-darwin-x86_64.tar.gz"
   sha256 "CHANGE_ME"
   license "GPL-3.0-or-later"
 
