29b1cb86273cd06534106e6f32f64098d162738c
Ofer Chen <oferchen@gmail.com>
2025-11-15 19:56:11 +0100

    chore: update Homebrew formula for v3.4.1-rust (#2159)
    Co-authored-by: oc-rsync-bot <actions@github.com>

--- Formula/oc-rsync.rb
+++ Formula/oc-rsync.rb
@@ -7,18 +7,17 @@ class OcRsync < Formula
   on_macos do
     on_intel do
       url "https://github.com/oferchen/rsync/releases/download/v3.4.1-rust/oc-rsync-3.4.1-rust-darwin-x86_64.tar.gz"
-      sha256 "9bf88cd605304b2b340a35a2e8366db5fde3fb048cf191f1b53d4256f8d3358c"
+      sha256 "1accf8e57076106ff64025c0f0eb8dba6a395360dba8b6caf1d6ae223ceb1c38"
     end
 
     on_arm do
       url "https://github.com/oferchen/rsync/releases/download/v3.4.1-rust/oc-rsync-3.4.1-rust-darwin-aarch64.tar.gz"
-      sha256 "27f6486516a6d2a113f79bbe55cbf9b5f8b552b5a94d91a5d91de8749bbfc30e"
+      sha256 "0dd6ccb12217bef7f424734e6323c5d8bc970cadc162daf4fc5f445c9ac94a33"
     end
   end
 
   def install
     bin.install "oc-rsync"
-    bin.install "oc-rsyncd"
   end
 
   test do
