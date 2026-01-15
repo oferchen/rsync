e6bff0fa6e764cd1b54f4b382ecd9c9571f34355
Ofer Chen <oferchen@gmail.com>
2026-01-15 16:59:05 +0100

    chore: update Homebrew formula for v0.5.2 (#2295)
    Co-authored-by: oc-rsync-bot <actions@github.com>

--- Formula/oc-rsync.rb
+++ Formula/oc-rsync.rb
@@ -7,12 +7,12 @@ class OcRsync < Formula
   on_macos do
     on_intel do
       url "https://github.com/oferchen/rsync/releases/download/v0.5.2/oc-rsync-0.5.2-darwin-x86_64.tar.gz"
-      sha256 "368486fcee03c81c9ee05bf80a1c06913342dde34e069c2e4d6255d76a7eff92"
+      sha256 "55c9f99015981772e79edd14d5fae5fd391d1118a22bbf7642835dde509cdc28"
     end
 
     on_arm do
       url "https://github.com/oferchen/rsync/releases/download/v0.5.2/oc-rsync-0.5.2-darwin-aarch64.tar.gz"
-      sha256 "a5b09e7384afef87de180cb3873374188d96728219499c89cde096b8ff764ea8"
+      sha256 "dd34a0e38380e97fb90a3c01492c149556e3b727312891255fc92b43337af6df"
     end
   end
 
